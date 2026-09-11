188438e174c37e3099db036d7f148f10fce18604
veigxz <marlisson.veiga@gmail.com>
2026-09-11 08:54:37 -0300

    feat(documentos): Validacao do arquivo

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -19,7 +19,10 @@ namespace BancoSENAIAPI.Controllers
         [HttpPost("upload/{codigoCliente}")]
         public async Task<IActionResult> AnexarArquivo(int codigoCliente, IFormFile arquivo)
         {
-               
+            if(arquivo == null || arquivo.Length == 0)
+            {
+                return BadRequest("Nenhum Arquivo foi enviado.");
+            }
         }
     }
 }
