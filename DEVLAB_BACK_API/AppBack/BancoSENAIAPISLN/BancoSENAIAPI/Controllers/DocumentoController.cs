9053847d586db268f3057e6a0735aa04d83ac6fa
veigxz <marlisson.veiga@gmail.com>
2026-09-11 08:37:24 -0300

    feat(documentos): Simulacao do bando de dados

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace BancoSENAIAPI.Controllers
 {
@@ -6,6 +7,13 @@ namespace BancoSENAIAPI.Controllers
     [Route("api/v1/[controller]")]
     public class DocumentoController : Controller
     {
-        
+        private readonly string _caminhoRaiz = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            "ClienteArquivos"
+        );
+
+        private static List<Models.DocumentoMetadado> _documentosMetadados = new List<Models.DocumentoMetadado>();
+
+        private static int _nextId = 1;
     }
 }
