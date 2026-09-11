ba8cd49d71cdf8c8c989d7cf7e8c43e499280551
veigxz <marlisson.veiga@gmail.com>
2026-09-11 08:30:17 -0300

    feat(documento) criacao do controller e model de documentos

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BancoSENAIAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DocumentoController : Controller
+    {
+
+    }
+}
