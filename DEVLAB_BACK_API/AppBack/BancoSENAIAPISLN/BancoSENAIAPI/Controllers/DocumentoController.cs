a2dac38ddd27017c9f4722303becd6650f45fbd5
veigxz <marlisson.veiga@gmail.com>
2026-09-11 09:24:15 -0300

    feat(documentos): Salvando arquivo no servidor

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -35,6 +35,11 @@ namespace BancoSENAIAPI.Controllers
             string novoNome = $"{codigoCliente}_{nomeOriginal}_{Guid.NewGuid()}{extensao}";
             string caminhoFinal = Path.Combine(pastaCliente, novoNome);
 
+            using (var stream = new FileStream(caminhoFinal, FileMode.Create))
+            {
+                await arquivo.CopyToAsync(stream);
+            }
+
         }
     }
 }
